dd1074c692d7d74cfa9755e4fe3faa0153a62ef2
Djuhar <djuharghm@gmail.com>
2025-08-04 11:53:51 +0700

    button state for "like button"

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
+import { useState } from "react"
+
 // Components
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
+
   return (
     <div className="card-container">
-      <Card title="Star Wars" />
+      <Card title="Star Wars" rating={5} isCool={true}  />
       <Card title="One Piece"/>
       <Card title="John Wick"/>
     </div>
