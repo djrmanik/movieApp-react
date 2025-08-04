b8234dcf563931469454f929b354f745204e5eb7
Djuhar <djuharghm@gmail.com>
2025-08-04 12:52:21 +0700

    Tailwind CSS setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,13 @@
-import { useState, useEffect } from "react"
-
-// Components
-const Card = ({ title }) => {
-  const [count, setCount] = useState(0);
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect( () => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
-
-  useEffect(() => {
-    console.log('CARD RENDERED')
-  }, []);
-
-
-  return (
-    <div className="card" onClick={() => setCount(count + 1)}>
-      <h2>{title} <br/> {count} </h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? "❤️" : "🤍"}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
-
-
   return (
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true}  />
-      <Card title="One Piece"/>
-      <Card title="John Wick"/>
+    <div>
+        <h1 class="text-3xl font-bold underline text-red-500">
+    Hello world!
+  </h1>
     </div>
   )
 }
 
-export default App;
+export default App
