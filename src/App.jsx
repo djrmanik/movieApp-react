332ac3a3c4625c1b01771c55934f51785a164326
Djuhar <djuharghm@gmail.com>
2025-08-05 21:29:59 +0700

    trending movies feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
 import Search from './components/Search.jsx'
 import Spinner from './components/Spinner.jsx';
 import MovieCard from './components/MovieCard.jsx';
 import { useEffect, useState } from 'react'
 import { useDebounce } from 'react-use';
+import { updateSearchCount } from './appwrite.js';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -49,7 +49,11 @@ const App = () => {
         setMovieList([]);
         return;
       }
-      setMovieList(data.results);
+
+      setMovieList(data.results || []);
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
 
     } catch (error) {
       console.error(`Error fetching movies: ${error}`);
